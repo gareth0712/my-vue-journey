23ca91d621f2770a5faeb1663b29789db064417b
gareth0712 <gareth_0712@hotmail.com>
2021-03-28 17:12:54 +0800

    Add solution for assignment of 08

---
+++ 08-componentCommunication/assignment/src/components/UserData.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <h2>Add User</h2>
+    <div>
+      <label>Name:</label>
+      <input v-model="username" />
+    </div>
+    <div>
+      <label>Age:</label>
+      <input v-model="age" />
+    </div>
+    <button @click.prevent="addUser">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      age: '',
+    };
+  },
+  methods: {
+    addUser() {
+      this.$emit('add-user', this.username, this.age);
+    },
+  },
+};
+</script>
