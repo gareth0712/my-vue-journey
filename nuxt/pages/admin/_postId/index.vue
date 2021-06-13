72ab431d08aa52078f04e88ee7e047b984d94501
gareth0712 <gareth_0712@hotmail.com>
2021-06-13 22:26:40 +0800

    Add layout, styling and methods for admin pages

---
+++ nuxt/pages/admin/_postId/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <admin-post-form :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import AdminPostForm from '@/components/Admin/AdminPostForm.vue';
+import Vue from 'vue';
+
+export default Vue.extend({
+  components: {
+    AdminPostForm,
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: 'Max',
+        title: 'My awesome post',
+        content: 'Good content',
+        thumbnailLink: '/code.jpg',
+      },
+    };
+  },
+  layout: 'admin',
+});
+</script>
+
+<style lang="scss" scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
---
+++ nuxt/pages/admin/new-post/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="admin-new-post-page">
+    <admin-post-form class="new-post-form" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import AdminPostForm from '@/components/Admin/AdminPostForm.vue';
+
+export default Vue.extend({
+  components: {
+    AdminPostForm,
+  },
+  layout: 'admin',
+});
+</script>
